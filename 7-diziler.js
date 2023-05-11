0b944d79d610f990be8e116dd17913f0f8e0335a
Vural Kayra Cetintas <73638860+vuralkayracetintas@users.noreply.github.com>
2023-05-11 23:16:17 +0300

    diziler islendi

---
+++ 7-diziler.js
@@ -0,0 +1,40 @@
+
+
+
+let urun1 = 'iphone 12';
+let urun2 = 'iphone 13';
+let urun3 = 'iphone 14';
+
+let urunler = ['iphone 12','iphone 13','iphone 14'];
+let fiyatlar = [1000,2000,3000];
+let renkler = ['kirmizi','mavi','yesil'];
+
+
+let urun4 = ['iphone 14',3000,'yesil'];
+let urun5 = ['iphone 14 pro',4000,'mor'];
+let urun6 = ['iphone 14 pro max',5000,'siyah'];
+let urun7 = [];
+let urun8 = ['iphone 11', 2020,['turuncu','sari','kahverengi']]
+
+urun7[0] = 'iphone 12 pro max';
+urun7[1] = 13540;    
+urun7[2] = 'beyaz';
+
+
+console.log(`cihaz : ${urunler[0]} fiyati : ${fiyatlar[0]} rengi : ${renkler[0]}`);
+console.log(`cihaz : ${urunler[1]} fiyati : ${fiyatlar[1]} rengi : ${renkler[1]}`);
+console.log(`cihaz : ${urunler[2]} fiyati : ${fiyatlar[2]} rengi : ${renkler[2]}`);
+
+console.log(urun4);
+console.log(urun5);
+console.log(urun6);
+console.log(urun7);
+console.log(urun8[2][1]);
+
+console.log(urunler[0]);
+console.log(urunler[1]);
+console.log(urunler[2]);
+
+let stringIfade = 'merhabalar javascript kursuna hosgeldiniz';
+console.log(stringIfade.split(' '));
+
